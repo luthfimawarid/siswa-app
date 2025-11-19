dabfeb1995981639ccf5286f70ec9ad9add08227
lutfhiemawarid@gmail.com <lutfhiemawarid@gmail.com>
2025-11-19 16:35:00 +0700

    bismillah

--- frankenphp.php
+++ frankenphp.php
@@ -1,17 +1,14 @@
 <?php
 
 return [
-    // Arahkan ke direktori public
     'document_root' => __DIR__ . '/public',
 
-    // Jalankan laravel
     'adapter' => \FrankenPhp\Laravel\LaravelAdapter::class,
 
-    // enable workers
     'workers' => [
         [
             'type' => 'http',
             'workers' => 2,
         ],
-    ]
+    ],
 ];
